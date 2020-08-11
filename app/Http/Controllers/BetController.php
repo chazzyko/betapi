8f7ee5248cb5eb1f44960f92357f066ffcb4e31f
Andy Mike <andy@jumper.lt>
2020-08-11 09:58:15 +0300

    style fixes

--- app/Http/Controllers/BetController.php
+++ app/Http/Controllers/BetController.php
@@ -1,7 +1,5 @@
 <?php
-
 namespace App\Http\Controllers;
-
 
 use App\Bet;
 use App\Player;
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,5 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,14 +12,6 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
-Route::middleware('api')->get('/user', function (Request $request) {
-    return 'Hello user muser';
-});
-
-//Route::middleware('api', 'throttle:60,1')->group(function () {
-//    Route::post('/bet', 'BetController@store');
-//});
 
 Route::middleware('api')->group(function () {
     Route::post('/bet', 'BetController@store');
